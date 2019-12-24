b7c50c14a5fc3c3c15c02c5449cc6e1e01124d75
liuchaoqun <liuchaoqun@guazi.com>
2019-12-24 19:54:56 +0800

    编辑距离

---
+++ src/main/java/dp/Q72.java
@@ -0,0 +1,19 @@
+package dp;
+
+/**
+ * @author chelsea
+ * @date 2019-12-24
+ * <p>
+ * 编辑距离
+ * 给定两个单词 word1 和 word2，计算出将 word1 转换成 word2 所使用的最少操作数 。
+ * 你可以对一个单词进行如下三种操作：
+ * 插入一个字符
+ * 删除一个字符
+ * 替换一个字符
+ */
+public class Q72 {
+
+    public int minDistance(String word1, String word2) {
+        return 0;
+    }
+}

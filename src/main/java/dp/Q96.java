d1143fbd270ffaa87eb98ae843a6123b924ad11a
liuchaoqun <liuchaoqun@guazi.com>
2019-12-24 20:02:30 +0800

    96.不同的二叉搜索树

---
+++ src/main/java/dp/Q96.java
@@ -0,0 +1,14 @@
+package dp;
+
+/**
+ * @author chelsea
+ * @date 2019-12-24
+ * <p>
+ * 不同的二叉搜索树：给定一个整数 n，求以 1 ... n 为节点组成的二叉搜索树有多少种？
+ */
+public class Q96 {
+
+    public int numTrees(int n) {
+        return 0;
+    }
+}
